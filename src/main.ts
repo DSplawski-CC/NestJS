92ae939c3e8a189a99fb7c1d434bad4a7e0a5890
Daniel Splawski <daniel.splawski@prores.no>
2025-03-10 15:01:20 +0100

    Added swagger for auto-generating API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Movies DB')
+    .setDescription('The MOVIES DB API description')
+    .setVersion('1.0')
+    .addTag('movies')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
