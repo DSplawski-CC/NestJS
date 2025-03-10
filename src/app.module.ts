cca92808b681a16d109ae02ed9e81fca938b37b1
Daniel Splawski <daniel.splawski@prores.no>
2025-03-10 09:23:46 +0100

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ReviewController } from './review/review.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController, ReviewController],
+  providers: [AppService],
+})
+export class AppModule {}
