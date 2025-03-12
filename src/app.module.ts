7f9f1b6194872ac6f599c37a3e0bb6de109211a5
Daniel Splawski <daniel.splawski@prores.no>
2025-03-12 16:17:48 +0100

    Created user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReviewModule } from '@modules/review/review.module';
+import { UserModule } from '@modules/user/user.module';
 
 
 @Module({
-  imports: [ReviewModule],
+  imports: [ReviewModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
