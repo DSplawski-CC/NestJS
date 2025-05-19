9db110c7643628420992b40f1362f8b4648d5790
Daniel Splawski <daniel.splawski@codeconcept.pl>
2025-05-19 15:24:53 +0200

    reconfiguration of eslint

---
+++ prettier.config.ts
@@ -0,0 +1,10 @@
+import { type Config } from 'prettier';
+
+const config: Config = {
+  trailingComma: 'none',
+  tabWidth: 2,
+  singleQuote: true,
+  endOfLine: 'auto',
+};
+
+module.exports = config;
